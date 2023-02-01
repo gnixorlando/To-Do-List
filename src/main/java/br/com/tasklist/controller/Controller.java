72ed25d515f2e5d373d3f897576caa316070eac6
gnixorlando <gabyorlandonascimento@gmail.com>
2023-02-01 07:18:49 -0300

    post method

--- src/main/java/br/com/tasklist/controller/Controller.java
+++ src/main/java/br/com/tasklist/controller/Controller.java
@@ -4,9 +4,13 @@ import java.lang.reflect.Method;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,14 +23,22 @@ public class Controller {
 	@Autowired
 	private TaskRepository taskRepository;
 	
-	@RequestMapping(value = "/initial/{taskname}", method = RequestMethod.GET)
-	@ResponseStatus(HttpStatus.OK)
-	public String initial (@PathVariable String taskname) {
-		Task task = new Task();
-		task.setTaskName(taskname);
+	// @RequestMapping(value = "/initial/{taskname}", method = RequestMethod.GET)
+	//@ResponseStatus(HttpStatus.OK)
+	//public String initial (@PathVariable String taskname) {
+		//Task task = new Task();
+		//task.setTaskName(taskname);
 		
-		taskRepository.save(task);
-		return "Task Created: " + taskname;
+		//taskRepository.save(task);
+		//return "Task Created: " + taskname; 
 	
+	//}
+	
+	@PostMapping(value = "save")
+	@ResponseBody
+	public ResponseEntity<Task> save (@RequestBody Task task) {
+		Task tsk = taskRepository.save(task);
+		return new ResponseEntity<Task>(tsk, HttpStatus.CREATED);
+		
 	}
 }
