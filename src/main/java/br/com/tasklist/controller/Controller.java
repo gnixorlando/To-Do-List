0ad923e9e80c39d250c35bcce39eddc2a9354c8a
gnixorlando <gabyorlandonascimento@gmail.com>
2023-02-10 09:13:05 -0300

    update method

--- src/main/java/br/com/tasklist/controller/Controller.java
+++ src/main/java/br/com/tasklist/controller/Controller.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -51,10 +53,22 @@ public class Controller {
 		return new ResponseEntity<List<Task>> (tasks, HttpStatus.OK);
 	}
 	
+	@DeleteMapping (value = "delete")
 	public ResponseEntity<String> delete (@RequestParam Long id) {
 		
 		taskRepository.deleteById(id);
 		return new ResponseEntity<String> ("successfully deleted task", HttpStatus.OK);
+	}
+	
+	@PutMapping(value="up")
+	@ResponseBody
+	public ResponseEntity<?> up (@RequestBody Task task) {
+		if (task.getId()==null) {
+				return new ResponseEntity<String> ("Id not informed!", HttpStatus.OK);
+				
+		}
+		Task tsk = taskRepository.saveAndFlush(task);
+		return new ResponseEntity<Task> (tsk, HttpStatus.CREATED);
 	}
 	
 }
